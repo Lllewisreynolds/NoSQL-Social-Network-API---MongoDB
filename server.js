f83feb26643fbbff5ce33024743c9600d754f7f4
Lllewisreynolds <lsreynolds108@gmail.com>
2024-02-13 12:25:16 +0000

    Boilerplate code for main server.js file created. Separate file named routes.js required here will contain Express route definitions - not yet defined

---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Database connection call and server start
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
